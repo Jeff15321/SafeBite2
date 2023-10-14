d1196f763b3de0395545028445964280644560f9
Elakkiyan <elakkiyan2006@gmail.com>
2023-10-14 17:09:02 -0400

    Accquired data to change website according to user input, and multi allergies.

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, url_for, request, redirect
 import json
+import os
 
-global allergy, restaurant
+dir = os.listdir("restaurant_menu")
+for i in range(len(dir)):
+    dir[i] = dir[i].replace(".json", "")
+    dir[i] = dir[i].replace("_", " ")
+
 
 
 app = Flask(__name__)
@@ -12,20 +17,20 @@ def home():
     if request.method == "POST":
         rest = request.form['restsearch'].lower()
         restaurant = rest
-        if rest=="taste of italy":
-            return redirect(url_for("italy"))
+        if rest in dir:
+            return redirect(url_for("store"))
         else:
             return redirect(url_for("user", rest=rest))
     else:
         return render_template("basic.html")
 
-@app.route("/tasteofitaly", methods=['POST','GET'])
-def italy():
+@app.route("/restaurant", methods=['POST','GET'])
+def store():
     global allergy
     if request.method == "POST":
-        allergy = request.form['allsearch'].lower()
+        allergy = request.form['allsearch'].lower().split(", ")
         check_allergy(restaurant, allergy)
-    return render_template("italy.html")
+    return render_template("redirect.html", restaurant=restaurant.title())
 
 @app.route("/<rest>")
 def user(rest):
@@ -39,7 +44,7 @@ def check_allergy(restaurnat, allergy):
 
     for menu_item in menu:
         for ingredient in menu[menu_item]:
-            if ingredient.lower() == allergy:
+            if ingredient.lower() in allergy and menu_item not in food_containing_allergy:
                 food_containing_allergy.append(menu_item)
 
     print(f"Food containing allergy: {food_containing_allergy}")
