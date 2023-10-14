4d5f31c78b2ea23779c4260f88dcddf5ae9377c1
Elakkiyan <elakkiyan2006@gmail.com>
2023-10-14 16:40:06 -0400

    Finished the identifying which dishes have respective allergies.

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template, url_for, request, redirect
+import json
+
+global allergy, restaurant
+
+
 app = Flask(__name__)
 
 @app.route("/", methods=['POST','GET'])
 def home():
+    global restaurant
     if request.method == "POST":
         rest = request.form['restsearch'].lower()
+        restaurant = rest
         if rest=="taste of italy":
             return redirect(url_for("italy"))
         else:
@@ -14,13 +21,28 @@ def home():
 
 @app.route("/tasteofitaly", methods=['POST','GET'])
 def italy():
+    global allergy
     if request.method == "POST":
         allergy = request.form['allsearch'].lower()
+        check_allergy(restaurant, allergy)
     return render_template("italy.html")
 
 @app.route("/<rest>")
 def user(rest):
     return f"<h1>We do not have {rest} as a restaurant.</hr>"
+
+def check_allergy(restaurnat, allergy):
+    food_containing_allergy = []
+    file = open(f"restaurant_menu/{restaurnat.replace(' ', '_')}.json", "r")
+    menu = json.load(file)
+    file.close()
+
+    for menu_item in menu:
+        for ingredient in menu[menu_item]:
+            if ingredient.lower() == allergy:
+                food_containing_allergy.append(menu_item)
+
+    print(f"Food containing allergy: {food_containing_allergy}")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- test.py
+++ test.py
@@ -1,16 +1,24 @@
 import json
 import os
+from main import *
+
 
 directory = "restaurant_menu"
 file = open("restaurant_menu/taste_of_italy.json", "r")
 menu = json.load(file)
 file.close()
 
+food_containing_allergy = []
+
 for menu_item in menu:
-    print(f"Menu Item: {menu_item}")
-    for ingredient_list in menu[menu_item]:
-        print(f"Ingredients: {ingredient_list}")
-    break
+    for ingredient in menu[menu_item]:
+        if ingredient.lower() == "eggs":
+            food_containing_allergy.append(menu_item)
+
+print(f"Food containing allergy: {food_containing_allergy}")
+
+
+
 
 
 
