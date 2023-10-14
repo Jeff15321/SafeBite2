f36b5acd20679294df49999738e241094b71e62d
Jackson Cai <jacksoncai200@gmaill.com>
2023-10-14 12:04:23 -0400

    Backend data and more search bars.

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['POST','GET'])
 def home():
-    return render_template("basic.html")
+    if request.method == "POST":
+        rest = request.form['restsearch'].lower()
+        if rest=="taste of italy":
+            return render_template("italy.html")
+        else:
+            return redirect(url_for("user", rest=rest))
+    else:
+        return render_template("basic.html")
+@app.route("/<rest>")
+def user(rest):
+    return f"<h1>We do not have {rest} as a restaurant.</hr>"
 
 if __name__ == "__main__":
     app.run(debug=True)
