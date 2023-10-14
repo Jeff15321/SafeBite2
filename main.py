53d115f29bfe34cd2a52dd599903b91012bee629
Jackson Cai <jacksoncai200@gmaill.com>
2023-10-14 13:32:47 -0400

    Access to other pages (italy) and reworked specific html files

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ def home():
     if request.method == "POST":
         rest = request.form['restsearch'].lower()
         if rest=="taste of italy":
-            return render_template("italy.html")
+            return redirect(url_for("italy"))
         else:
             return redirect(url_for("user", rest=rest))
     else:
         return render_template("basic.html")
+
+@app.route("/tasteofitaly")
+def italy():
+    return render_template("italy.html")
+
 @app.route("/<rest>")
 def user(rest):
     return f"<h1>We do not have {rest} as a restaurant.</hr>"
